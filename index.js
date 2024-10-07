4022f292f51edf826905974c4b2c0722c7bbe6fe
Pushpendraraj7879 <pushpendraraj7879@gmail.com>
2024-10-07 22:09:20 +0530

    complete login signup

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const cookieParser=require("cookie-parser");
 const {setUser,getUser}=require("./utils/auth");
 const checkAuthentication=require("./middlewares/authentication");
 
-app.use(checkAuthentication);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
@@ -21,8 +20,8 @@ app.use(express.static(path.join(__dirname,"public")));
 
 async function connectDB(){
     try{
-       await mongoose.connect(process.env.MONGO_URL);
-       console.log("database is successful connected")
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("database is successful connected")
     }catch(error){
         console.log("error in database",error);
     }
@@ -30,34 +29,52 @@ async function connectDB(){
 
 connectDB();
 
+// app.use(checkAuthentication);
 
 
 
 
 
 
-app.get('/signup',(req,res)=>{
+app.get('/signup',checkAuthentication,(req,res)=>{
+    res.render("signup");
+})
+
+app.get('/login',checkAuthentication,(req,res)=>{
     res.render("login");
 })
 
-app.get('/login',(req,res)=>{
-    res.render("login");
+app.get('/home',(req,res)=>{
+    const uid=req.cookies?.uid;
+    
+    
+    if(!uid)return res.redirect('/login');
+    
+    
+    const user=getUser(uid);
+    
+    
+
+    if(!user)return res.redirect('/login');
+    res.render("home");
 })
 
 app.post("/signup-data",async(req,res)=>{
-     const {firstName,lastName,email,passoword}=req.body;
+     const {firstName,lastName,email,password}=req.body;
+     console.log(req.body);
 
      
      try{
-        const user= await User.findOne({"email":email});
+        // const user= await User.findOne({"email":email});
 
-        if(user) return res.render("/signup",{message:"you are already registered"});
+        // if(user) return res.redirect("/signup");
 
-        await User.create({firstName,lastName,email,passoword});
+        await User.create({firstName,lastName,email,password});
         console.log("successfully data is submitted to database");
         const token=setUser(firstName,process.env.SECRET_KEY);
         res.cookie("uid",token);
         res.redirect('/home');
+        
      }catch(error){
         console.log("there is some error ",error);
      }
@@ -70,12 +87,13 @@ app.post("/login-data",async(req,res)=>{
 
     try{
        const user= await User.findOne({"email":email,"password":password});
-       if(!user) return res.render("/login",{message:"you are not registered"});
+       if(!user) return res.redirect("/login");
          console.log("successfully data is submitted to database");
-      
+         const token=setUser(user.firstName,process.env.SECRET_KEY);
+         res.cookie("uid",token);
        res.redirect('/home');
     }catch(error){
-       console.log("there is some error ",error);
+       console.log("there is  error ",error);
     }
 
 })
--- utils/auth.js
+++ utils/auth.js
@@ -5,10 +5,11 @@ const setUser=(data,secret)=>{
         return jwt.sign(data,secret);
 }
 
-const getUser=(token,secret)=>{
+const getUser=(token)=>{
     if(!token)return null;
+    // console.log(token);
     try{
-   return jwt.verify(token,secret)
+   return jwt.verify(token,process.env.SECRET_KEY)
     }catch(error){
         return null;
     }
